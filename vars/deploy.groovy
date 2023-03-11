2937cc6da7337400a0eddeb5f0f8403432ddd392
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-11 22:31:33 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -3,7 +3,7 @@ def call(Map config = [:]) {
   def namespace = config.namespace ?: 'default'
   def deploymentName = config.deploymentName
   def yamlFilePath = config.yamlFilePath
-  def labels = config.labels ?: [:]
+ 
 
   sh "kubectl create ns ${namespace}"
   sh "kubectl apply -f ${yamlFilePath} -n ${namespace}"
