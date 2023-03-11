97a91729fc6ca7974ef84265fb71602ffa5d7175
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-11 13:09:51 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++
@@ -1,5 +0,0 @@
-def VERSION = 'rohitkothapalli'
-def call(String dockerfilePath = '/Users/krvnbangarraju/.jenkins/workspace/cicd-task/', String imageName = 'my-image', String imageTag = 'latest') {
-  def dockerBuildCmd = "docker build -t ${imageName}:${imageTag} ${dockerfilePath}"
-  sh "${dockerBuildCmd}"
-}
