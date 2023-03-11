12dc5a793c3cd866b50f2631a08bdc39296f6a3f
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-11 14:43:44 +0530

    Update testMaven.groovy

--- vars/testMaven.groovy
+++ vars/testMaven.groovy
@@ -1,6 +1,4 @@
 def VERSION = 'rohitkothapalli'
 def call() {
-    def mvnHome = tool 'Maven' // Set the Maven installation
-    def mavenCommand = "${mvnHome}/bin/mvn" // Set the Maven command
-    sh "${mavenCommand} test" // Run Maven test command
+    sh 'mvn test'
 }
