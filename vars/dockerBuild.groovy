8e5b2fc4fffd4a9c5d16e5c550c8a269326b46a7
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-11 13:51:51 +0530

    Update dockerBuildAndPush.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,34 @@
+def VERSION = 'rohitkothapalli'
+
+def call(Map pipelineParams) {
+  // Define some default values for the pipeline parameters
+  def dockerfilePath = pipelineParams.dockerfilePath ?: '/Users/krvnbangarraju/.jenkins/workspace/cicd-task/Dockerfile'
+  def buildArgs = pipelineParams.buildArgs ?: ''
+  def dockerImageName = pipelineParams.dockerImageName ?: 'my-docker-image'
+  def dockerImageTag = pipelineParams.dockerImageTag ?: 'latest'
+  def dockerRegistryUrl = pipelineParams.dockerRegistryUrl ?: 'docker.io'
+  def dockerRegistryUsername = pipelineParams.dockerRegistryUsername ?: ''
+  def dockerRegistryPassword = pipelineParams.dockerRegistryPassword ?: ''
+
+  // Define the Docker build command
+  def dockerBuildCmd = "docker build -t ${dockerImageName}:${dockerImageTag} -f ${dockerfilePath} ${buildArgs} ."
+
+  // Log the Docker build command to the Jenkins console
+  echo "Building Docker image: ${dockerBuildCmd}"
+
+  // Execute the Docker build command
+  sh "${dockerBuildCmd}"
+
+  // Log the Docker push command to the Jenkins console
+  def dockerPushCmd = "docker push ${dockerImageName}:${dockerImageTag}"
+  echo "Pushing Docker image: ${dockerPushCmd}"
+
+  // Execute the Docker push command
+  withCredentials([usernamePassword(credentialsId: 'docker-registry-creds', passwordVariable: 'DOCKER_REGISTRY_PASSWORD', usernameVariable: 'DOCKER_REGISTRY_USERNAME')]) {
+    sh """
+      docker login ${dockerRegistryUrl} -u ${dockerRegistryUsername} -p ${DOCKER_REGISTRY_PASSWORD}
+      ${dockerPushCmd}
+      docker logout
+    """
+  }
+}
