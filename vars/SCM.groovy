2b456b83335edb066ddf6d597c60ddd332154bf8
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-11 09:49:57 +0530

    Update SCM.groovy

--- vars/SCM.groovy
+++ vars/SCM.groovy
@@ -1,34 +1,4 @@
 def VERSION = 'rohitkothapalli'
-def call(String scmType, String scmUrl) {
-    switch (scmType.toLowerCase()) {
-        case 'git':
-            checkoutGit(scmUrl)
-            break
-        case 'svn':
-            checkoutSvn(scmUrl)
-            break
-        case 'mercurial':
-            checkoutMercurial(scmUrl)
-            break
-        default:
-            throw new IllegalArgumentException("Unsupported SCM type: ${scmType}")
-    }
-}
-
-def checkoutGit(String scmUrl) {
-    stage('Checkout (Git)') {
-        checkout([$class: 'GitSCM', branches: [[name: '*/*']], userRemoteConfigs: [[url: scmUrl]]])
-    }
-}
-
-def checkoutSvn(String scmUrl) {
-    stage('Checkout (SVN)') {
-        checkout([$class: 'SubversionSCM', locations: [[credentialsId: 'my-svn-creds', remote: scmUrl]], workspaceUpdater: [$class: 'UpdateUpdater']])
-    }
-}
-
-def checkoutMercurial(String scmUrl) {
-    stage('Checkout (Mercurial)') {
-        checkout([$class: 'MercurialSCM', credentialsId: 'my-hg-creds', source: scmUrl])
-    }
+def call() {
+  git: 'https://github.com/rohitkothapalli/task-repo'
 }
