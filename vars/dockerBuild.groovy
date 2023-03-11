2c5682e71bc19134c243f0fca75602f2d0166396
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-11 12:57:20 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,34 +1,5 @@
 def VERSION = 'rohitkothapalli'
-// Define a new pipeline step for building a Docker image
-def call(Map pipelineParams) {
-  // Define some default values for the pipeline parameters
-  def dockerfilePath = pipelineParams.dockerfilePath ?: '/Users/krvnbangarraju/.jenkins/workspace/cicd-task/Dockerfile'
-  def buildArgs = pipelineParams.buildArgs ?: ''
-  def dockerImageName = pipelineParams.dockerImageName ?: 'my-docker-image'
-  def dockerImageTag = pipelineParams.dockerImageTag ?: 'latest'
-  def dockerRegistryUrl = pipelineParams.dockerRegistryUrl ?: 'https://registry.hub.docker.com'
-  def dockerRegistryUsername = pipelineParams.dockerRegistryUsername ?: 'krvnb'
-  def dockerRegistryPassword = pipelineParams.dockerRegistryPassword ?: 'RohiT.123'
-
-  // Define the Docker build command
-  def dockerBuildCmd = "docker build -t ${dockerImageName}:${dockerImageTag} -f ${dockerfilePath} ${buildArgs} ."
-
-  // Log the Docker build command to the Jenkins console
-  echo "Building Docker image: ${dockerBuildCmd}"
-
-  // Execute the Docker build command
+def call(String dockerfilePath = '.', String imageName = 'my-image', String imageTag = 'latest') {
+  def dockerBuildCmd = "docker build -t ${imageName}:${imageTag} ${dockerfilePath}"
   sh "${dockerBuildCmd}"
-
-  // Log the Docker push command to the Jenkins console
-  def dockerPushCmd = "docker push ${dockerImageName}:${dockerImageTag}"
-  echo "Pushing Docker image: ${dockerPushCmd}"
-
-  // Execute the Docker push command
-  withCredentials([usernamePassword(passwordVariable: 'DOCKER_REGISTRY_PASSWORD', usernameVariable: 'DOCKER_REGISTRY_USERNAME')]) {
-    sh """
-      docker login ${dockerRegistryUrl} -u ${dockerRegistryUsername} -p ${dockerRegistryPassword}
-      ${dockerPushCmd}
-    """
-  }
- 
 }
