0ab6014c817cbc4e52bcaef0df26077548dfa461
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-13 14:32:06 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -2,6 +2,8 @@ def VERSION = 'rohitkothapalli'
 def call(String pomPath, String mavenOpts = '') {
     withEnv(['MAVEN_OPTS='+mavenOpts]) {
         stage('Build (Maven)') {
+            sh "apt-get update"
+            sh "apt-get install maven"
             sh "mvn -f ${pomPath} clean install"
         }
     }
