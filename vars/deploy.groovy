74b7ba4b1976a56856ff70ede46afa483475125a
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-14 11:32:55 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def VERSION = 'rohitkothapalli'
 def call(Map config = [:]) {
-  def namespace = config.namespace ?: 'default'
+//   def namespace = config.namespace ?: 'default'
   def deploymentName = config.deploymentName
   def yamlFilePath = config.yamlFilePath
  
